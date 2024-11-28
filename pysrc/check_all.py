7744e6987a07dc70fb38906717ae65150475acac
Max Ward <maxhwardg@gmail.com>
2024-11-28 18:36:45 +0800

    Checking and benchmarking code between packages

--- pysrc/check_all.py
+++ pysrc/check_all.py
@@ -4,28 +4,35 @@ from lineardesign_bridge import call_lineardesign
 import protein
 import vienna
 
+def validate_res(seq, mfe, db, eps, alg):
+    ctx = vienna.ViennaContext(seq, dangles=0)
+    vienna_fe = ctx.free_energy(db)
+    vienna_mfe = ctx.free_energy(ctx.mfe())
+    assert abs(vienna_fe - mfe) < eps, f'{vienna_fe} != {mfe} for {alg}'
+    assert abs(vienna_mfe - mfe) < eps, f'{vienna_mfe} != {mfe} for {alg}'
+    
+
 def main():
     eps = 1e-3
-    aa_len = 50
+    aa_len = 30
     cft = protein.CodonFrequencyTable('../data/homosapiens.txt')
     for _ in range(1000):
         aa_seq = protein.random_aa_seq(aa_len)
         print(aa_seq)
-        res = call_cdsfold("../extern/CDSfold-main", aa_seq)
-        assert len(res.rna_seq) == len(aa_seq)*3, f'{len(res.rna_seq)} != {len(aa_seq)*3}'
-        assert len(res.rna_seq) == len(res.db), f'{len(res.rna_seq)} != {len(res.db)}'
-        cds = protein.rna_to_cds(res.rna_seq)
-        for i, codon in enumerate(cds):
-            assert codon in cft.get_codons(aa_seq[i]), f'{codon} not in {cft.get_codons(aa_seq[i])} for amino acid {aa_seq[i]} (index {i})'
-        print(res)
-        ctx = vienna.ViennaContext(res.rna_seq, dangles=0)
-        vienna_fe = ctx.free_energy(res.db)
-        vienna_mfe = ctx.free_energy(ctx.mfe())
-        print(res.mfe, vienna_fe, vienna_mfe)
-        assert abs(vienna_fe - res.mfe) < eps, f'{vienna_fe} != {res.mfe}'
-        assert abs(vienna_mfe - res.mfe) < eps, f'{vienna_mfe} != {res.mfe}'
-        ref_cai = cft.codon_adaptation_index(cds)
-        print(ref_cai)
+        cds_res = call_cdsfold("../extern/CDSfold-main", aa_seq)
+        linear_res = call_lineardesign(cft, "../extern/LinearDesign-main/", aa_seq)
+        derna_res = call_derna(cft, "../extern/derna-main", aa_seq)
+        
+        
+        print(cds_res.mfe, linear_res.mfe, derna_res.mfe)
+        assert abs(cds_res.mfe - linear_res.mfe) < eps, f'{cds_res.mfe} != {linear_res.mfe}'
+        assert abs(cds_res.mfe - derna_res.mfe) < eps, f'{cds_res.mfe} != {derna_res.mfe}'
+        
+        validate_res(cds_res.rna_seq, cds_res.mfe, cds_res.db, eps, 'cdsfold')
+        validate_res(linear_res.rna_seq, linear_res.mfe, linear_res.db, eps, 'lineardesign')
+        validate_res(derna_res.rna_seq, derna_res.mfe, derna_res.db, eps, 'derna')
+        
+        
         
 
 if __name__ == '__main__':
